dd93d4f2d4b93b92c1b4d524ef01972c03e251f9
Michiel de Jong <michiel@unhosted.org>
2020-10-02 11:55:39 +0200

    add testers

--- README.md
+++ README.md
@@ -49,6 +49,8 @@ Run the following commands in your command line terminal:
 docker build -t websockets-pubsub testers/websockets-pubsub
 docker build -t rdf-fixtures testers/rdf-fixtures
 docker build -t webid-provider testers/webid-provider
+docker build -t solid-crud testers/solid-crud
+docker build -t web-access-control testers/web-access-control
 docker build -t cookie helpers/cookie
 
 docker build -t table-reporter reporters/table
--- runTests.sh
+++ runTests.sh
@@ -2,37 +2,6 @@ echo Testing $1 ...
 echo Building image ...
 docker build -t $1 servers/$1
 
-# echo Starting server without WAC ...
-# docker run -d --name=server --env SKIP_WAC=true --network=testnet $1
-# 
-# if [[ "$1" == trellis ]]
-#   then
-#     docker logs server
-#     echo Waiting for ten seconds ...
-#     sleep 10
-#     docker logs server
-# fi
-# 
-# if [[ "$1" == nextcloud-server ]]
-#   then
-#     sleep 10
-#     docker logs server
-#     echo Running init script for Nextcloud server ...
-#     docker exec -u www-data -it server sh /init.sh
-#     docker exec -u root -it server service apache2 reload
-#     echo Getting cookie...
-#     export COOKIE="`docker run --rm --cap-add=SYS_ADMIN --network=testnet --name cookie --env-file servers/$1/env.list cookie`"
-# fi
-# 
-# echo Running ldp-basic tester ...
-# docker run --network=testnet ldp-basic > reports/$1-ldp-basic.txt
-# 
-# echo Stopping server without WAC ...
-# docker stop server
-# 
-# echo Removing server without WAC ...
-# docker rm server
-
 echo Starting server with WAC ...
 docker run -d --name=server --network=testnet $1
 
@@ -55,17 +24,17 @@ if [[ "$1" == nextcloud-server ]]
     export COOKIE="`docker run --rm --cap-add=SYS_ADMIN --network=testnet --name cookie --env-file servers/$1/env.list cookie`"
 fi
 
-# echo Running websockets-pubsub tester ...
-# docker run --rm --network=testnet websockets-pubsub 2> reports/$1-websockets-pubsub.txt
-# 
 # echo Running rdf-fixtures tester ...
 # docker run --rm --network=testnet rdf-fixtures > reports/$1-rdf-fixtures.txt
 
 echo Running webid-provider tester with cookie $COOKIE...
 docker run --rm --network=testnet --name tester --env COOKIE="$COOKIE" --env-file servers/$1/env.list webid-provider 2> reports/$1-webid-provider.txt
 
-echo Running webid-provider tester ...
-docker run --network=testnet --env-file servers/$1/env.list webid-provider 2> reports/$1-webid-provider.txt
+echo Running solid-crud tester with cookie $COOKIE...
+docker run --rm --network=testnet --name tester --env COOKIE="$COOKIE" --env-file servers/$1/env.list solid-crud 2> reports/$1-solid-crud.txt
+
+echo Running web-access-control tester with cookie $COOKIE...
+docker run --rm --network=testnet --name tester --env COOKIE="$COOKIE" --env-file servers/$1/env.list web-access-control 2> reports/$1-web-access-control.txt
 
 echo Stopping server with WAC ...
 docker stop server
